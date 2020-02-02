064568f44565fa0cda8af0556507610308210eeb
Th3-attacker <47328817+Th3-attacker@users.noreply.github.com>
2020-02-02 21:36:00 +0000

    Update ajouter_client.c

--- ajouter_client.c
+++ ajouter_client.c
@@ -6,33 +6,32 @@
 void ajouter_client()
 {
     int nombre_client;
-
+    system("cls");
     printf("Entrez le nombre de client a ajouter: ");
     scanf("%d", &nombre_client);
-
-    client C[nombre_client];
-
+    system("cls");
     char reponse[5];
 
     for (int i = 0; i < nombre_client; i++)
     {
-        printf("[NNI]      : ");
-        scanf("%s", C[i].nni);
+        client C;
+        printf("Client %d [NNI]      : ", i + 1);
+        scanf("%s", C.nni);
 
-        printf("[PRENOM]   : ");
-        scanf("%s", C[i].prenom);
+        printf("Client %d [PRENOM]   : ", i + 1);
+        scanf("%s", C.prenom);
 
-        printf("[NOM]      : ");
-        scanf("%s", C[i].nom);
+        printf("Client %d [NOM]      : ", i + 1);
+        scanf("%s", C.nom);
 
-        printf("[TEL]      : ");
-        scanf("%s", C[i].tel);
+        printf("Client %d [TEL]      : ", i + 1);
+        scanf("%s", C.tel);
 
-        printf("[PROFESSION]: ");
-        scanf("%s", C[i].profession);
+        printf("Client %d [PROFESSION]: ", i + 1);
+        scanf("%s", C.profession);
 
-        printf("[DATE_NAISSANCE] (j/m/a): ");
-        scanf("%s", C[i].date_naiss);
+        printf("Client %d [DATE_NAISSANCE] (j/m/a): ", i + 1);
+        scanf("%s", C.date_naiss);
 
         compte cmp;
 
@@ -41,15 +40,15 @@ void ajouter_client()
 
         printf("ENREGISTRE (o/n): ");
         scanf("%s", reponse);
-
         if (strcmp("o", reponse) == 0)
         {
-            enregistre_client(C);
-            cree_compte(&cmp, C);
+            enregistre_client(&C);
+            cree_compte(&cmp, &C);
         }
         else
         {
             continue;
         }
+        system("cls");
     }
 }
